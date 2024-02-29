7a31147fcc116247e657e6bb63b827e6d5286077
a777511471 <389033813@qq.com>
2024-02-29 21:52:55 +0800

    我很可爱

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
-
+import openpyxl as op
+from pathlib import Path
+import shutil
+import pyperclip
 
 # 这是一个示例 Python 脚本。
 # 按 Shift+F10 执行或将其替换为您的代码。
 # 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+
+SRC_PATH = Path.absolute(Path(__file__)).parent
+
+
+# file_path = str(SRC_PATH / "a/b.ui")
+# todo: 不要引相对路径。打包时会有问题，把所有相对路径都改成上面的格式。
+
+class OpeningUI(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = None
+        self.init_ui()
+
+    def init_ui(self):
+        self.ui = uic.loadUi(str(SRC_PATH / "./main.ui"))
+
+
 class MainUI(QMainWindow):
     # 幽默主界面
     # todo 美化
@@ -70,6 +90,16 @@ class MainTable_import_goods(QWidget):
         # todo 补
 
 
+class MainTable_PM(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = None
+        self.init_ui()
+
+    def init_ui(self):
+        self.ui = uic.loadUi(str(SRC_PATH / "./main.ui"))
+
+
 def print_hi(name):
     # 在下面的代码行中使用断点来调试脚本。
     print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
@@ -79,10 +109,11 @@ def print_hi(name):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
+    opening = OpeningUI()
     main_ui = MainUI()
     main_table1 = MainTable_export_goods()
     main_table2 = MainTable_import_goods()
-    main_ui.ui.show()
-    
-    app.exec()
+    main_table3 = MainTable_PM()
+    opening.ui.show()
 
+    app.exec()
